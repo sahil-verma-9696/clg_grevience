1ee2471d86d77efcc75e189439658504e676cbf1
sahil-verma-9696 <laptopsahil123@gmail.com>
2023-09-14 21:38:52 +0530

    today i initialize the git in this folder and today i get data from form in node js server by both get/post method of form

---
+++ 14-09/form-actions/server.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//
+
+const app = express();
+
+// here we parse the body of url into json using body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true}));
+
+const server = http.createServer(app);
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");// this line serve index.html on localhost:3000
+})
+
+// by this method we get the data of form 
+// app.get("/form-action",(req,res)=>{
+//     console.log(req.query)
+//     res.send(req.query);
+// })
+
+// getting data from form when form use post method to send data ..
+
+app.post("/form-action",(req,res)=>{ // /form-action use because i give "/form-action" value in "action" attribute of form in html side..
+
+    res.send(req.body)// post method data send in body which is sequre and slow also
+    // res.send(req.query)
+})
+server.listen(3000,()=>{
+    console.log("http://localhost:3000");
+})
